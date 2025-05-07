845ad41fc20ef0d11a552d045c7d01a9dd07da4f
dwiratomo12 <dwiratomo@gmail.com>
2025-05-07 11:06:41 +0700

    continued submit test

--- app/Http/Controllers/StudentAnswerController.php
+++ app/Http/Controllers/StudentAnswerController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CourseQuestion;
 use App\Models\StudentAnswer;
+use App\Models\Course;
+use App\Models\CourseAnswer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
 
 class StudentAnswerController extends Controller
 {
@@ -26,9 +32,71 @@ class StudentAnswerController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Course $course, $question)
     {
-        //
+        
+        $question_details = CourseQuestion::where('id', $question)->first();
+
+        $validated = $request->validate([
+            'answer_id' => 'required|exists:course_answers,id',
+        ]);
+
+        DB::beginTransaction();
+
+        try{
+            $selectedAnswer = CourseAnswer::find($validated['answer_id']);
+
+            // Check if the selected answer belongs to the question
+            if($selectedAnswer->course_question_id != $question){
+                $error = ValidationException::withMessages([
+                    'system_error' => ['System error!' .['Jawaban tidak tersedia dengan pertanyaan']],
+                ]);
+
+                throw $error;
+            }
+            // Check if the question is already answered
+            $existingAnswer = StudentAnswer::where('user_id', Auth::id())
+                ->where('course_question_id', $question)
+                ->first();
+            if ($existingAnswer) {
+                $error = ValidationException::withMessages([
+                    'system_error' => ['System error!' .['Kamu telah menjawab pertanyaan ini sebelumnya']],
+                ]);
+
+                throw $error;
+            }
+
+            $answerValue = $selectedAnswer->is_correct ? 'correct' : 'wrong';
+            StudentAnswer::create([
+                'user_id' => Auth::id(),
+                'course_question_id' => $question,
+                'answer' => $answerValue,
+            ]);
+            
+            DB::commit();
+
+            $nextQuestion = CourseQuestion::where('course_id', $course->id)
+                ->where('id', '>', $question)
+                ->orderBy('id','asc')
+                ->first();
+
+            if($nextQuestion){
+                return redirect()->route('dashboard.learning.course', ['course' => $course->id, 'question' => $nextQuestion->id])
+                ->with('success', 'Jawaban kamu sudah disimpan, silahkan lanjut ke pertanyaan selanjutnya');
+            } else {
+                return redirect()->route('dashboard.learning.finished.course', ['course' => $course->id])
+                ->with('success', 'Jawaban kamu sudah disimpan, silahkan lihat raport belajar kamu');
+            }
+
+            
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $error = ValidationException::withMessages([
+                'system_error' => ['System error!' . $e->getMessage()],
+            ]);
+
+            throw $error;
+        }
     }
 
     /**
